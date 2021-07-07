cff659092c1d9ffe72e0a49c9d0248c299788cc8
Edgar Niño <edgarnio@live.com>
2021-07-07 18:27:30 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 
 const app = express();
-app.set('port', 5000);
+app.set('port', process.env.PORT||5000);
 
 app.use(morgan('dev'));
 
